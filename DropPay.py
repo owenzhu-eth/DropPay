05efab999be57e81ec7e663e6145b1b7f5cf90a1
Owen Zhu <72231596+owenzhu78@users.noreply.github.com>
2021-05-17 23:04:14 -0400

    Update DropPay.py

--- DropPay.py
+++ DropPay.py
@@ -4,22 +4,22 @@ By OwenZhu78#0001
 """
 
 def DropPay():
-    price_in=float(input("Price of item you want to sell at? "))
-    category_in=str(input("Category of item (electronic,console,media,other)? "))
-    shipping_in=int(input("Estimated shipping cost? "))
-    profit_in=float(input("Profit margin you want to make (enter as decimal)? "))
+    price=float(input("Price of item you want to sell at? "))
+    category=str(input("Category of item (electronic,console,media,other)? "))
+    shipping=int(input("Estimated shipping cost? "))
+    profit=float(input("Profit margin you want to make (enter as decimal)? "))
 
-    if category_in.lower() == "electronic":
-        if price_in < 101:
-            payout=(price_in-(price_in*0.08)-shipping_in-(price_in*profit_in))
-        elif price_in > 100:
-            payout=(price_in-(((price_in-100)*0.08)+15)-shipping_in-(price_in*profit_in))
-    elif category_in.lower() == "console":
-        payout=(price_in-(price_in*0.08)-shipping_in-(price_in*profit_in))
-    elif category_in.lower() == "media":
-        payout=(price_in-((price_in*0.08)+1.80)-shipping_in-(price_in*profit_in))
-    elif category_in.lower() == "other":
-        payout=(price_in-(price_in*0.15)-shipping_in-(price_in*profit_in))
+    if category.lower() == "electronic":
+        if price < 101:
+            payout=(price-(price*0.08)-shipping-(price*profit))
+        elif price > 100:
+            payout=(price-(((price-100)*0.08)+15)-shipping-(price*profit))
+    elif category.lower() == "console":
+        payout=(price-(price*0.08)-shipping-(price*profit))
+    elif category.lower() == "media":
+        payout=(price-((price*0.08)+1.80)-shipping-(price*profit))
+    elif category.lower() == "other":
+        payout=(price-(price*0.15)-shipping-(price*profit))
 
     print(payout)
 
