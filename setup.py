b0e53318fa44a6019231bcacc5a3df23dfb6e6f0
Owen Zhu <72231596+owenzhu78@users.noreply.github.com>
2021-05-23 11:34:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ from setuptools import setup
 APP=['DropPay.py']
 OPTIONS={
   'argv_emulation': True,
+  'packages':['certifi'],
 }
 
 setup(
   app=APP,
   options={'py2app':OPTIONS},
-  setup_requires=['py2app']
+  setup_requires=['py2app'],
 )
