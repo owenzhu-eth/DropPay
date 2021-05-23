7312a54a7711ed2ffd95ef60f12cff077a2b6f61
Owen Zhu <72231596+owenzhu78@users.noreply.github.com>
2021-05-23 11:27:22 -0400

    Update DropPay.py

--- DropPay.py
+++ DropPay.py
@@ -5,7 +5,7 @@ By OwenZhu78#0001
 
 def DropPay():
     price=float(input("Price of item you want to sell at? "))
-    category=str(input("Category of item (electronic,console,media,other)? "))
+    category=str(input("Category of item (electronic,media,other)? "))
     shipping=float(input("Estimated shipping cost? "))
     profit=float(input("Profit margin you want to make (enter as decimal)? "))
 
@@ -14,8 +14,6 @@ def DropPay():
             payout=(price-(price*0.08)-shipping-(price*profit))
         elif price > 100:
             payout=(price-(((price-100)*0.08)+15)-shipping-(price*profit))
-    elif category.lower() == "console":
-        payout=(price-(price*0.08)-shipping-(price*profit))
     elif category.lower() == "media":
         payout=(price-((price*0.08)+1.80)-shipping-(price*profit))
     elif category.lower() == "other":
