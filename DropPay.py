fb6ece454f0f534a330abe40903f16445575b270
Owen Zhu <72231596+owenzhu78@users.noreply.github.com>
2021-07-01 00:10:19 -0400

    50

--- DropPay.py
+++ DropPay.py
@@ -8,7 +8,9 @@ def DropPay():
     category=str(input("Category of item (electronic,media,other)? "))
     shipping=float(input("Estimated shipping cost? "))
     profit=float(input("Profit margin you want to make (enter as decimal)? "))
-
+    payA=0
+    payH=0
+    
     if category.lower() == "electronic":
         if price < 101:
             payout=(price-(price*0.08)-shipping-(price*profit))
@@ -18,8 +20,39 @@ def DropPay():
         payout=(price-((price*0.08)+1.80)-shipping-(price*profit))
     elif category.lower() == "other":
         payout=(price-(price*0.15)-shipping-(price*profit))
-
-    print("Payout is " + str(round(payout)))
+        
+    print("")
+    print("Client payout is " + str(round(payout)))
     print("Profit is " + str(round(price*profit,2)))
 
-DropPay()
+
+def Payability():
+    price=float(input("Price of item you want to sell at? "))
+    category=str(input("Category of item (electronic,media,other)? "))
+    shipping=float(input("Estimated shipping cost? "))
+    payout=int(input("Payout to client? "))
+    payA=0
+    payH=0
+    
+    if category.lower() == "electronic":
+        if price < 101:
+            payA=(price-(price*0.02)-(price*0.08)-shipping)*0.8
+            payH=(price-(price*0.02)-(price*0.08)-shipping)*0.2
+        elif price > 100:
+            payA=(price-(price*0.02)-(((price-100)*0.08)+15)-shipping)*0.8
+            payH=(price-(price*0.02)-(((price-100)*0.08)+15)-shipping)*0.2
+    elif category.lower() == "media":
+        payA=(price-(price*0.02)-((price*0.08)+1.80)-shipping)*0.8
+        payH=(price-(price*0.02)-((price*0.08)+1.80)-shipping)*0.2
+    elif category.lower() == "other":
+        payA=(price-(price*0.02)-(price*0.15)-shipping)*0.8
+        payH=(price-(price*0.02)-(price*0.15)-shipping)*0.2
+
+    print("")
+    print("Payability advance is " + str(round(payA,2)))
+    print("Payability hold is " + str(round(payH,2)))
+    if payout>payA:
+        print("Out of pocket payout is " + str(round((payout-payA),2)))
+    else:
+        print("No out of pocket payout.")
+
